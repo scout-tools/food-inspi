f8047985d9ce5e568df3aebcff98c225d4968511
Robert Bagdahn <robertbaggi@gmail.com>
2022-11-10 21:16:39 +0100

    improved package.json

--- src/router.ts
+++ src/router.ts
@@ -2,23 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import AppRouter from '@/modules/app/router'
 import DashboardRouter from '@/modules/dashboard/router'
-import RecipeRouter from '@/modules/recipe/router'
-import IngredientRouter from '@/modules/ingredient/router'
-import FaqRouter from '@/modules/faq/router'
 import SettingsRouter from '@/modules/settings/router'
-import HintRouter from '@/modules/hint/router'
-import SupportRouter from '@/modules/support/router'
 import AuthRouter from '@/modules/auth/router'
 
 const routes = [
   ...DashboardRouter,
   ...AppRouter,
-  ...IngredientRouter,
-  ...RecipeRouter,
-  ...FaqRouter,
   ...SettingsRouter,
-  ...HintRouter,
-  ...SupportRouter,
   ...AuthRouter,
 ]
 
