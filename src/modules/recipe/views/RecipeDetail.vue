d45506b0df314c5c8cd09d3089dace89c7dc6674
Robert Bagdahn <robertbaggi@gmail.com>
2022-11-14 21:53:45 +0100

    added simulatorbutton in rezeptseite

--- src/modules/recipe/views/RecipeDetail.vue
+++ src/modules/recipe/views/RecipeDetail.vue
@@ -4,8 +4,24 @@
       <!-- Breadcrumb -->
       <Breadcrumbs :pages="pages" />
       <article>
-        <RecipeOverview 
-          :recipeItems="recipeDetail.recipeItems"/>
+        <RecipeOverview :recipeItems="recipeDetail.recipeItems" />
+        <div
+          class="
+            px-2
+            py-2
+            sm:flex sm:items-center sm:justify-between sm:px-6
+            lg:px-8
+          "
+        >
+          <div class="min-w-0 flex-1"></div>
+          <div class="mt-4 flex-2 sm:mt-0 sm:ml-4">
+            <PrimaryButton
+              color="blue"
+              label="Simulieren"
+              @click="onSimulateClicked(recipeDetail.id)"
+            />
+          </div>
+        </div>
       </article>
     </main>
   </div>
@@ -14,10 +30,14 @@
 <script setup lang="ts">
 import { onMounted, computed } from "vue";
 import Breadcrumbs from "@/components/breadcrumbs/Header.vue";
+import PrimaryButton from "@/components/button/Primary.vue";
 import RecipeItemList from "@/modules/recipe/components/RecipeItemList.vue";
 import RecipeOverview from "@/modules/recipe/components/RecipeMainOverview.vue";
 import { useRoute } from "vue-router";
 import { useRecipeStore } from "@/modules/recipe/store/index.ts";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 
 const route = useRoute();
 
@@ -33,6 +53,16 @@ const recipeStore = useRecipeStore();
 const recipeDetail = computed(() => {
   return recipeStore.recipeDetail;
 });
+
+async function onSimulateClicked(id) {
+  const response = await recipeStore.cloneRecipe(id);
+  if (response && response.status == 201) {
+    router.push({
+      name: "SimulatorMain",
+      params: { id: response.data.id },
+    });
+  }
+}
 
 onMounted(() => {
   const id = route.params.id;
